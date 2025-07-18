9eaf1b1a797e7107b5150439c5877034ee7d63ae
Ahmedabdelaziz77 <zozahmed770@gmail.com>
2025-07-19 02:47:40 +0300

    setup frontend, add base components and landing page

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; 2025 ELZOZ; All Rights Reserved.</p>
+      <div className="footer__links">
+        {["About", "Privacy Policy", "Licensing", "Contact"].map((item) => (
+          <Link
+            href={`/${item.toLowerCase().replace(" ", "-")}`}
+            key={item}
+            className="footer__link"
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
